3fbfd19f842ed50cab8e520a69e4e7d88f136e27
Ivana Mihalek <ivana.mihalek@gmail.com>
2020-01-08 20:38:52 +0100

    mating

--- 06_make_progeny.py
+++ 06_make_progeny.py
@@ -20,10 +20,28 @@
 # add some 10% de novo mutations
 # make sure that they are not disease causing
 
+# reminder: interpreting GT field
+# GT represents the genotype, encoded as allele values separated by / or |.
+# If the allele value is 0, means that it is equal to the reference allele
+# (what is in REF field), if 1 mean that is equal to alternative (first allele listed in ALT),
+#  and if 2 it is equal to the second allele listed in ALT (if it exists).
+# Thus GT = 1/1 represents a SNP homozygous for the ALT allele,
+# 1/0 heterozygous, and 0/0 homozygous for the reference
+# The files at hand never list the second ALT, so I suspect that two alleles, neither
+# one of which is reference, are given something like this:
+
+#chr	676339	.	A	G	.	PASS	.	GT	0/1
+#chr	676339	.	AAC	A	.	PASS	.	GT	1/0
 
 ############################
 def main():
 	vcfdir  = "/storage/sequencing/openhumans/fakexomes"
+	maledir   = "{}/males".format(vcfdir)
+	femaledir = "{}/females".format(vcfdir)
+	for dep in [vcfdir, maledir, femaledir]:
+		if not os.path.exists(dep):
+			print(dep, "not found")
+			exit()
 	return
 
 
